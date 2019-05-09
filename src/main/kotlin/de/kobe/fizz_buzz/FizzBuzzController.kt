0bc057d1f4085e1a394d4971c32788b250731105
Kobe <ingo.stoecker@gmail.com>
2019-05-09 22:48:20 +0200

    use service and its calculation implementation

--- src/main/kotlin/de/kobe/fizz_buzz/FizzBuzzController.kt
+++ src/main/kotlin/de/kobe/fizz_buzz/FizzBuzzController.kt
@@ -4,23 +4,11 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/fizz-buzz")
-class FizzBuzzController {
+class FizzBuzzController (private val fizzBuzzService: FizzBuzzService) {
 
     @GetMapping(value = ["/{value}"])
     @ResponseBody
     fun fizzBuzz(@PathVariable value: Int): String {
-        return calculate(value)
-    }
-
-    private fun calculate(value: Int): String {
-        return if (value % 3 == 0 && value % 5 ==0) {
-            "$value fizz buzz"
-        } else if (value % 3 == 0) {
-            "$value fizz"
-        } else if (value % 5 == 0) {
-            "$value buzz"
-        } else {
-            "$value"
-        }
+        return fizzBuzzService.calculate(value)
     }
 }
