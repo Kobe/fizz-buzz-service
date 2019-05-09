1bf306563664aeb93369fb5c4b1c810f65185163
Kobe <ingo.stoecker@gmail.com>
2019-05-09 22:50:20 +0200

    formatting

--- src/main/kotlin/de/kobe/fizz_buzz/FizzBuzzService.kt
+++ src/main/kotlin/de/kobe/fizz_buzz/FizzBuzzService.kt
@@ -6,14 +6,11 @@ import org.springframework.stereotype.Service
 class FizzBuzzService {
 
     fun calculate(value: Int): String {
-        return if (value % 3 == 0 && value % 5 ==0) {
-            "$value fizz buzz"
-        } else if (value % 3 == 0) {
-            "$value fizz"
-        } else if (value % 5 == 0) {
-            "$value buzz"
-        } else {
-            "$value"
+        return when {
+            value % 15 == 0 -> "$value fizz buzz"
+            value % 3 == 0 -> "$value fizz"
+            value % 5 == 0 -> "$value buzz"
+            else -> "$value"
         }
     }
 }
