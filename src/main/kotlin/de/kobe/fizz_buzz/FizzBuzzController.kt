3e6cc364d5f811be0d2211e31f8741c16ff09604
Kobe <ingo.stoecker@gmail.com>
2020-05-14 23:43:58 +0200

    #5 simplify controller
    @ResponseBody not required due to @RestController

--- src/main/kotlin/de/kobe/fizz_buzz/FizzBuzzController.kt
+++ src/main/kotlin/de/kobe/fizz_buzz/FizzBuzzController.kt
@@ -2,6 +2,7 @@ package de.kobe.fizz_buzz
 
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
@@ -11,13 +12,8 @@ class FizzBuzzController (
     private val fizzBuzzService: FizzBuzzService
 ) {
     @GetMapping(value = ["/{value}"], produces = [APPLICATION_JSON_VALUE])
-    @ResponseBody
-    fun getFizzBuzzResult(@PathVariable value: Int): ResponseEntity<FizzBuzzResult> {
-        return ResponseEntity
-                .status(200)
-                .body(FizzBuzzResult(
-                        value = value,
-                        result = fizzBuzzService.calculate(value)))
+    fun getFizzBuzzResult(@PathVariable value: Int): FizzBuzzResult {
+        return FizzBuzzResult(value = value, result = fizzBuzzService.calculate(value))
     }
 }
 
