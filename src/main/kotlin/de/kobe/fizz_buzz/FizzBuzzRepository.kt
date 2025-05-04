b65fcc9d9a861863c107ce5be4ed6c0a9dbb1b84
Kobe <81110+Kobe@users.noreply.github.com>
2025-05-04 21:29:17 +0200

    bump spring boot

--- src/main/kotlin/de/kobe/fizz_buzz/FizzBuzzRepository.kt
+++ src/main/kotlin/de/kobe/fizz_buzz/FizzBuzzRepository.kt
@@ -4,7 +4,6 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 
 // https://spring.io/guides/tutorials/spring-boot-kotlin/
@@ -13,7 +12,7 @@ interface FizzBuzzRepository: CrudRepository<FizzBuzzResult, UUID> {}
 
 @Entity
 data class FizzBuzzResult (
-    @Id @GeneratedValue
+    @Id
     val id: UUID = UUID.randomUUID(),
     val timestamp: Long,
     val dateTimeBerlin: String,
